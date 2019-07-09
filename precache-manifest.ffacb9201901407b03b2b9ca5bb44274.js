fcbe32cc3ed6fcd0e7aec9509b6218d7a45e2ec5
Dawid Mucha <dawidmucha98@gmail.com>
2019-07-09 12:31:58 +0200

    Updates

---
+++ precache-manifest.ffacb9201901407b03b2b9ca5bb44274.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da4f6bae877d49e7eed856e5e28acca",
+    "url": "/true-or-false/index.html"
+  },
+  {
+    "revision": "c9710d984bd771927eaf",
+    "url": "/true-or-false/static/css/main.4d0a3e14.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/true-or-false/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "c9710d984bd771927eaf",
+    "url": "/true-or-false/static/js/main.b0f81dac.chunk.js"
+  },
+  {
+    "revision": "f6c6a9a54ba033862eb9",
+    "url": "/true-or-false/static/js/runtime~main.35534bef.js"
+  }
+]);
